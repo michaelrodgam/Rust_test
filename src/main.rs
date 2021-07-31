97701ed572908e45f0c8584e531fbce6f4f52677
michael <michaelrodgam@gmail.com>
2021-07-31 00:13:27 -0600

    Updated version, after modify the access to the functions examples

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 
 //import
-mod print;
+mod functions;
 
 //main function
 fn main() {
+
+    
     println!("Start--");
-    print::run();
-    print::conditionals();
-    print::loops();
-    print::enums_testing();
+    println!("Input a number, to show different results");
+    
+    //call the fn input_line to get the user choice
+    let choice = functions::input_line();
+    println!("-input choice-");
+
+    //parse the choice from String to u32
+    let num_choice: u32 = choice.parse().unwrap();
+
+    //Control flow with diferrent options for the user.
+    match num_choice{
+        0 => {
+            functions::run();
+        },
+        1 => {
+            functions::loops();
+        },
+        2 => {
+            functions::enums_testing();
+        },
+        3 => {
+            functions::conditionals();
+        },
+        _ => {
+
+        }
+    }
+
     println!("--End");
 }
